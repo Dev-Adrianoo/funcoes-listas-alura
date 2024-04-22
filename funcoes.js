8d31400e20cc5078ff103e930212b5142f96d62d
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-04-22 19:05:49 -0300

    atividade de função

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+function pegarcafe() {
+  let levantarDaCadeira = true;
+  let caminharAteaCozinha = true;
+  if (levantarDaCadeira && caminharAteaCozinha) {
+    console.log("Me Levantei e caminhei até a cozinha.");
+  } 
+}
