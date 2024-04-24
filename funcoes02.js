e0a274bc670f8dd8665dd1580a006232a94d35a8
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-04-23 21:37:06 -0300

    exercicíos

---
+++ funcoes02.js
@@ -0,0 +1,10 @@
+const nome = Adriano;
+const salario = 1200;
+
+const tempoDeCasa = 5;
+
+let novoSalario = (salario * 12) / 100
+
+const resultado = novoSalario + salario;
+
+alert(`Seu Nome ${nome} seu salário ${salario} seu tempo trabalhando conosco ${tempoDeCasa} seu aumento será de 12% ${resultado}`)
---
+++ semaforo.js
@@ -0,0 +1,9 @@
+const corDoFarol = ["verde", "vermelho", "roxo"];
+
+if (corDoFarol[0] == "verde") {
+  console.log("Atravesse");
+} else if (corDoFarol[1] == "vermelho") {
+  console.log("Não Atravesse");
+} else {
+  console.log("Sinal inoperante");
+}
